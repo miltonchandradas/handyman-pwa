51e6966810a2386b76ed8d84e1ebdfcb7cf8c8c6
Milton Chandradas <milton.chandradas@live.com>
2021-09-16 11:28:13 -0400

    Push notification...

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const PROJECTS_PATH = "users";
 
 export const NODE_BASE_URL =
    "https://handyman-node-restless-toucan-tk.cfapps.eu10.hana.ondemand.com/";
-export const SUBSCRIPTION_PATH = "subscription";
+export const SUBSCRIPTION_PATH = "api/v1/subscription";
 
 export const PUBLIC_VAPID_KEY =
    "BM262ZEnEU1k3iWfobSYPilZf76qd5NyEODJEjExR0LF-MJXVp6qJBv4eEJzzYWS7QwskJmqQUqnpY0xGvqJ3ns";
