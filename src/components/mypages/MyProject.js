7c8a3c46ff3cf556b683277c7158b6878abf6c99
Milton Chandradas <milton.chandradas@live.com>
2021-09-17 18:46:52 -0400

    Push notification...

--- src/components/mypages/MyProject.js
+++ src/components/mypages/MyProject.js
@@ -22,8 +22,8 @@ const MyProject = ({ project }) => {
                project.description ? project.description : ""
             }`}</Label>
             <Label>{`Cost: ${project.estimatedCost}`}</Label>
-            <Label>{`Start Date: ${project.projectedStartDate}`}</Label>
-            <Label>{`End Date: ${project.projectedEndDate}`}</Label>
+            <Label>{`Start Date: ${project.projectedStartDate.toLocaleDateString()}`}</Label>
+            <Label>{`End Date: ${project.projectedEndDate.toLocaleDateString()}`}</Label>
          </div>
          <Button>Rehire Handyman</Button>
       </Card>
